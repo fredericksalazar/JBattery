6be7b4fa2c3c22361b6239dccf789140c266e07c
FrederickSalazar <fredefass01@gmail.com>
2018-11-30 21:48:46 -0500

    Create the JBUtil object for admin the utilities process

---
+++ src/jbattery/core/util/JBUtil.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jbattery.core.util;
+
+/**
+ *
+ * @author frederick
+ */
+public class JBUtil {
+    
+    private final String nameApp;
+    private final String version;
+    private final String autor;
+    private final String build;
+    
+    public static JBUtil instance;
+    
+    private JBUtil(){
+        this.autor = "Frederick Salazar";
+        this.nameApp = "JBattery";
+        this.version = "2.2";
+        this.build = "20182230";
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public String getBuild() {
+        return build;
+    }
+
+    public String getNameApp() {
+        return nameApp;
+    }    
+    
+    public String getNameAppVersionBuild(){
+        return this.nameApp+" Version "+this.version+" BUILD "+this.build;
+    }
+    public static JBUtil getInstance(){
+        if(instance == null){
+            instance = new JBUtil();
+        }
+        
+        return instance;
+    }
+    
+}
