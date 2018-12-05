d71b4ba49f2083f2f7e967284ec848a68ec229b0
FrederickSalazar <fredefass01@gmail.com>
2018-12-04 22:33:24 -0500

    JBUtil create new methos of util used in Jbattery

--- src/jbattery/core/util/JBUtil.java
+++ src/jbattery/core/util/JBUtil.java
@@ -6,9 +6,13 @@
 package jbattery.core.util;
 
 /**
- *
+ * This class define the main methods that are used as tools in Jbattery
+ * Project
+ * 
  * @author frederick
+ * 
  */
+
 public class JBUtil {
     
     private final String nameApp;
@@ -16,13 +20,15 @@ public class JBUtil {
     private final String autor;
     private final String build;
     
+    private String OS;
+    
     public static JBUtil instance;
     
     private JBUtil(){
         this.autor = "Frederick Salazar";
         this.nameApp = "JBattery";
-        this.version = "2.2";
-        this.build = "20181130";
+        this.version = "2.8";
+        this.build = "20181204";
     }
 
     public String getVersion() {
@@ -41,9 +47,57 @@ public class JBUtil {
         return nameApp;
     }    
     
+    
+    /**
+     * This method return the name and version information of Jbatter project
+     * @return 
+     */
+    public String getNameAppVersion(){
+        return this.nameApp+" "+this.version;
+    }
+    
+    /**
+     * this method return the name, version and build information
+     * of the Jbattery project
+     * 
+     * @return String 
+     */
+    
     public String getNameAppVersionBuild(){
         return this.nameApp+" Version "+this.version+" BUILD "+this.build;
     }
+    
+    /**
+     * Return de the operative System 
+     * @return 
+     */
+    public String getOS(){
+        if(OS == null || "".equals(OS)){
+            OS = System.getProperty("os.name");
+            System.out.println("The Operative System is : "+OS);
+        }
+        return OS;
+    }
+    
+    /**
+     * This Method verify the operative system of execution, in this moment
+     * only supported Linux OS
+     * @return 
+     */
+    
+    public boolean verifyOperativeSystem(){
+        boolean isSupported = true;        
+           if(!getOS().equals("Linux")){
+               isSupported = false;
+           } 
+        
+        return isSupported;        
+    }
+    
+    /**
+     * Create a instance of JBUtil object in the memory
+     * @return 
+     */
     public static JBUtil getInstance(){
         if(instance == null){
             instance = new JBUtil();
